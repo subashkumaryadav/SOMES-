60e2cf0e7e01d570f62017d5c3ab2a3278e865c6
subash-0 <subash67335@gmail.com>
2023-06-20 16:23:50 +0545

    subash-iv

--- src/pages/notes/Notes.js
+++ src/pages/notes/Notes.js
@@ -9,12 +9,33 @@ import { Link } from "react-router-dom";
 const Notes = () => {
   return (
     <Layout>
-      <h2 className={style.note_head}>BE MECHANICAL STUDY MATERIALS</h2>
+      <div className={style.note_head}>
+        <h3>BE MECHANICAL STUDY MATERIALS</h3>
+        <select
+          className={`form-select form-select-sm`}
+          aria-label=".form-select-sm example"
+          id="semValue"
+        >
+          <option value="1001">Select Your Semester</option>
+          <option value="1001">I Sem</option>
+          <option value="1002">II Sem</option>
+          <option value="1003">III Sem</option>
+          <option value="1004">IV Sem</option>
+          <option value="1005">V Sem</option>
+          <option value="1006">VI Sem</option>
+          <option value="1007">VII Sem</option>
+          <option value="1008">VIII Sem</option>
+         </select>
+      </div>
+     
       {notes.map((current) => {
         const note = current.year;
+       
         return (
           <div className={style.semeter_St} key={current.id}>
-            {note.map((data) => {
+            { 
+
+              note.map((data) => {
               return (
                 <div className={style.note_main} key={data.id}>
                   <h3>{data.sem}</h3>
@@ -23,13 +44,11 @@ const Notes = () => {
                     {data.Subjects.map((sub) => {
                       return (
                         <div
-                    
                           key={sub.id}
-                          className={`card shadow-lg p-3 bg-body mb-5 fade-in-left ${style.subjects}` }
+                          className={`card shadow-lg p-3 bg-body mb-5 fade-in-left ${style.subjects}`}
                           style={{ width: "18rem", height: "11rem" }}
                         >
                           <div className="card-header text-bold ">
-                           
                             <CiBookmarkCheck /> {sub.name}
                           </div>
                           <div className="card-body">
