04904aeb3da021a61e62789b6fd850865e49f075
subash-0 <subash67335@gmail.com>
2023-06-21 11:14:26 +0545

    search complete

--- src/pages/alumni/Form.js
+++ src/pages/alumni/Form.js
@@ -4,6 +4,7 @@ const Form = () => {
   return (
     <div className="form">
       <iframe
+        title="mygoogleform"
         src="https://docs.google.com/forms/d/e/1FAIpQLSeKqL3ALBT7TA2spZf3lqMs1yUoRzql2wFw1SC1Wnb0M-Azfw/viewform?embedded=true"
         width="800"
         height="1721"
