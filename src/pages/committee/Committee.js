6795f941b5d549a73d4c15dbc47ec54b12e65600
subashkumaryadav <078bct087@ioepc.edu.np>
2023-07-11 18:37:07 +0545

    datachange

--- src/pages/committee/Committee.js
+++ src/pages/committee/Committee.js
@@ -1,13 +1,63 @@
 import React from "react";
 import Layout from "../../components/layout/Layout";
 import { Link } from "react-router-dom";
-
+import { FaFacebook, FaPhone,FaVoicemail } from "react-icons/fa";
+import './committee.css';
 const Committee = () => {
-  return <Layout title={"Committee-SOMES"}>This is committee
-    <Link  to="tel:+977-9811202751"  >Fone</Link>
-    <br />
-    <Link  to="mailto:subashwp2@gmail.com"  >email</Link>
+  return <Layout title={"Committee-SOMES"}>
+     <div id='model3'>
+        <h1 className='model-title'>Team Model 3</h1>
+        <div className="divider"></div>
+        <div className="members">
+          <div className="member">
+            <img width={200} height={200} src=""/>
+            <div className="description">
+                <h1>Ahmed ALAMI</h1>
+                <h2>CEO</h2>
+                <p>
+                Consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat. Lorem ipsum dolor sit amet consectet.
+                </p>
+                <div className="social-media">
+                  <FaFacebook />
+                  <FaPhone />
+                  <FaVoicemail />
+                </div>
+            </div>
+          </div>
+          <div className="member">
+            <img width={200} height={200} src={"Member2"}/>
+            <div className="description">
+                <h1>Omar Nadir</h1>
+                <h2>CEO</h2>
+                <p>
+                Consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat. Lorem ipsum dolor sit amet consectet.
+                </p>
+                <div className="social-media">
+                  <FaFacebook />
+                  <FaPhone />
+                  <FaVoicemail />
+                </div>
+            </div>
+          </div>
+          <div className="member">
+            <img width={200} height={200} src={"Member3"}/>
+            <div className="description">
+                <h1>Khaled MAHER</h1>
+                <h2>CEO</h2>
+                <p>
+                Consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat. Lorem ipsum dolor sit amet consectet.
+                </p>
+                <div className="social-media">
+                  <FaFacebook />
+                  <FaPhone />
+                  <FaVoicemail />
+                </div>
+            </div>
+          </div>
+        </div>
+    </div>
   </Layout>;
 };
 
 export default Committee;
+
